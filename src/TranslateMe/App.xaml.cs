b53fb3744b70218fa6a9c271876b9d5d3f584d48
Tobias Persson <tobper@gmail.com>
2014-03-13 19:38:02 +0100

    Added handling of NotImplementedExceptions

--- src/TranslateMe/App.xaml.cs
+++ src/TranslateMe/App.xaml.cs
@@ -1,8 +1,29 @@
-﻿using System.Windows;
+﻿using System;
+using System.Windows;
+using System.Windows.Threading;
+using TranslateMe.Properties;
 
 namespace TranslateMe
 {
     public partial class App : Application
     {
+        public App()
+        {
+            DispatcherUnhandledException += OnDispatcherUnhandledException;
+        }
+
+        private static void OnDispatcherUnhandledException(object sender, DispatcherUnhandledExceptionEventArgs e)
+        {
+            if (e.Exception is NotImplementedException)
+            {
+                MessageBox.Show(
+                    "The requested feature has unfortunately not yet been implemented.",
+                    Strings.MessageBoxTitle,
+                    MessageBoxButton.OK,
+                    MessageBoxImage.Exclamation);
+
+                e.Handled = true;
+            }
+        }
     }
 }
