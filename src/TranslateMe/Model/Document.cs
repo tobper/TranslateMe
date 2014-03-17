52448b1f8f4c4f7e7b3b340b0b6890a0682cce10
Tobias Persson <tobper@gmail.com>
2014-03-17 11:57:05 +0100

    Cleanup

--- src/TranslateMe/Model/Document.cs
+++ src/TranslateMe/Model/Document.cs
@@ -1,9 +1,7 @@
-﻿using System;
-using System.Collections.ObjectModel;
+﻿using System.Collections.ObjectModel;
 using System.Collections.Specialized;
 using System.Globalization;
 using System.Linq;
-using System.Net.Sockets;
 
 namespace TranslateMe.Model
 {
