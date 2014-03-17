ba9826118d09c4e5650f2bd0f86c2fa94a2b031d
Tobias Persson <tobper@gmail.com>
2014-03-17 10:50:22 +0100

    Set document as not modified after load

--- src/TranslateMe/UI/Windows/MainWindow.xaml.cs
+++ src/TranslateMe/UI/Windows/MainWindow.xaml.cs
@@ -179,6 +179,7 @@ namespace TranslateMe.UI.Windows
             else
             {
                 CreateNewDocument(workingDirectory, documentName);
+                IsDocumentModified = false;
             }
         }
 
